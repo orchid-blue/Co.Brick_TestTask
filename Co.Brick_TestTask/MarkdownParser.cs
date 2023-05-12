cf73d0c77745d64a1a5313be1ad5fe619af6a5d2
Dasha Karpach <karpach.dasha.pl@gmail.com>
2023-05-12 16:38:37 +0200

    [Question 2] Add a Markdown Parser
    Write a simple markdown parser function that will take in a single line of markdown and translate it into the appropriate HTML.
    To keep it simple, support only one feature of markdown in atx syntax: headers. Headers are designated by 1-6 hash characters followed by a space, followed by text. The number of hashes determines the header level of the HTML output.
    Header content should only come after the initial hashtag(s) plus a space character. Invalid headers should just be returned as the markdown that was received, no translation necessary. Spaces before the hashes and after the header should be kept in the output, but between the hashes and the text only one space is allowed.

---
+++ Co.Brick_TestTask/MarkdownParser.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Co.Brick_TestTask
+{
+    public class MarkdownParser
+    {
+        public string GetFormattedText(string input) => ParseMarkup(input);
+
+        private string ParseMarkup(string input)
+        {
+            string pattern = @"^(\#{1,6}) (.*)";
+            Regex rx = new Regex(pattern);
+            MatchCollection matches = rx.Matches(input);
+            Match? match = matches.FirstOrDefault();
+
+            if (match == null || match.Groups.Count != 3)
+            {
+                return input;
+            }
+
+            string hashtags = match.Groups[1].Value;
+            string inputText = match.Groups[2].Value;
+
+            if (inputText[0] == ' ')
+            {
+                return input;
+            }
+
+            return GetTextWithTags(hashtags, inputText);
+        }
+
+        private string GetTextWithTags(string hashtags, string text)
+        {
+            string[] tags = GetTags(hashtags);
+
+            return $"{tags[0]}{text}{tags[1]}";
+        }
+
+        private string[] GetTags(string hashtags)
+        {
+            int hashtagsCount = hashtags.Length;
+
+            return new string[] { $"<h{hashtagsCount}>", $"</h{hashtagsCount}>" };
+        }
+    }
+}
