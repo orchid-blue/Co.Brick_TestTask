754d9c5a32de1aa9109c895140021e5093005401
Dasha Karpach <karpach.dasha.pl@gmail.com>
2023-05-12 14:33:33 +0200

    [Question 1] Get the middle character of the string
    Given a string, return the middle character of the string. If the string&#39;s length is
    odd, return the middle character. If the string&#39;s length is even, return the middle
    2 characters.

---
+++ Co.Brick_TestTask/StringHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Co.Brick_TestTask
+{
+    public static class StringHandler
+    {
+        public static string GetMiddleCharacter(string input)
+        {
+            int length = input.Length;
+            if (length == 1 || length == 2)
+            {
+                return input;
+            }
+
+            int wholePart = length / 2;
+            string result = input[wholePart].ToString();
+
+            bool isEven = (length % 2) == 0;
+            if (isEven)                
+            {
+                return input[wholePart - 1] + result;
+            }
+
+            return result;
+        }
+    }
+}
