41ab250e67f210aa3e73f38168c54dba39dc2769
Dasha Karpach <karpach.dasha.pl@gmail.com>
2023-05-12 16:40:42 +0200

    [Question 2] Add unit tests

---
+++ Co.Brick_Tests/MarkdownParserTest.cs
@@ -0,0 +1,22 @@
+﻿using Co.Brick_TestTask;
+
+namespace Co.Brick_Tests
+{
+    public class MarkdownParserTest
+    {
+        [Test]
+        [TestCase("<h1>Header</h1>", "# Header")]
+        [TestCase("<h2>Header</h2>", "## Header")]
+        [TestCase("<h6>Header</h6>", "###### Header")]
+        [TestCase("####### Header", "####### Header")]
+        [TestCase("###  Header", "###  Header")]
+        [TestCase("Header", "Header")]
+        [TestCase("", "")]
+        [TestCase("<h4># Header</h4>", "#### # Header")]
+        public void GetFormattesTest(string expected, string input)
+        {
+            MarkdownParser mp = new MarkdownParser();
+            Assert.That(mp.GetFormattedText(input), Is.EqualTo(expected));
+        }
+    }
+}
