ca6cae4e9aa6bee17259a57fba4213a835226c32
Dasha Karpach <karpach.dasha.pl@gmail.com>
2023-05-13 18:25:47 +0200

    [Question 3] Refactor the method
    The method was renamed for better readability.
    The logic of the method was updated to avoid unnecessary checks.
    The possibility of passing any type as a string was restricted.
    Only types from newly introduced Enum are possible.

--- Co.Brick_TestTask/Refactoring.cs
+++ Co.Brick_TestTask/Refactoring.cs
@@ -1,36 +1,17 @@
-﻿using System;
-public class Class1 { 
-    public static bool IsFormat(string str, string f)
+﻿using Co.Brick_TestTask;
+
+public class TypeFormatValidator 
+{ 
+    public static bool CanBeParsed(string str, FormatType type)
     {
-        var allowedDict = new Dictionary<string, bool>()
+        return type switch
         {
-            { "number",true},
-            { "date",true},
-            { "timespan",true}
+            FormatType.Number => int.TryParse(str, out var _),
+            FormatType.Date => DateTime.TryParse(str, out var _),
+            FormatType.TimeSpan => TimeSpan.TryParse(str, out var _),
+            _ => throw new Exception($"Unknown format type '{type}'"),
         };
-
-        int isNotAllowed = 0;
-        for (var i = 1; i < allowedDict.Count; i++)
-        {
-            if (f == allowedDict.Keys.ToArray()[i - 1])
-            {
-                isNotAllowed |= 1 << i;
-            }
-        }
-
-        if (isNotAllowed > 0)
-            throw new Exception("Format not allowed.");
-
-        if (f.ToLower() == "number")
-            return Int32.TryParse(str, out var _);
-        else if (f.ToLower() == "date")
-            return DateTime.TryParse(str, out var _);
-        else if (f.ToLower() == "timespan")
-            return DateTime.TryParse(str, out var _);
-
-        throw new Exception("Unknown format.");
     }
-
 }
 
 
